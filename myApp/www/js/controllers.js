c7b191d88c7835145f1766526b1be505aa7fd43f
LinaChan <cln3033162@163.com>
2017-09-25 17:36:17 +0800

    完成货物特性与IBC/IGC要求模块

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -31,21 +31,6 @@ angular.module('starter.controllers', [])
   })
    .controller('dangerGoodDetailCtrl', function($scope,$stateParams,$http,$q,$state) {
      $scope.goodName = $stateParams.goodName;
-     var defer = $q.defer();
-     $http({
-       method: 'get',
-       url: './templates/info.json'
-     }).success(function (data) {
-       defer.resolve(data);
-       for(var i=0;i<data.info.length;i++){
-         console.log(data.info[i]);
-         if(data.info[i].ChineseName==$scope.goodName){
-           $scope.detailInfo = data.info[i];
-         }
-       }
-     }).error(function (data) {
-       defer.reject(data);
-     })
      $scope.goToProperty = function (goodName) {
        $state.go('goodproperty',{goodNameForProperty:$stateParams.goodName});
      }
@@ -58,11 +43,39 @@ angular.module('starter.controllers', [])
   })
   .controller('goodPropertyCtrl', function($scope,$stateParams,$http,$q,$state) {
     $scope.goodName = $stateParams.goodNameForProperty;
-
+    var defer = $q.defer();
+    $http({
+      method: 'get',
+      url: './templates/info.json'
+    }).success(function (data) {
+      defer.resolve(data);
+      for(var i=0;i<data.info.length;i++){
+        if(data.info[i].ChineseName==$scope.goodName){
+          $scope.detailInfo = data.info[i].property;
+          console.log($scope.detailInfo);
+        }
+      }
+    }).error(function (data) {
+      defer.reject(data);
+    })
   })
   .controller('IBCIGCCtrl', function($scope,$stateParams,$http,$q,$state) {
     $scope.goodName = $stateParams.goodNameIBCIGC;
-
+    var defer = $q.defer();
+    $http({
+      method: 'get',
+      url: './templates/info.json'
+    }).success(function (data) {
+      defer.resolve(data);
+      for(var i=0;i<data.info.length;i++){
+        if(data.info[i].ChineseName==$scope.goodName){
+          $scope.detailInfo = data.info[i].IBCIGC;
+          console.log($scope.detailInfo);
+        }
+      }
+    }).error(function (data) {
+      defer.reject(data);
+    })
   })
   .controller('oilFenceCtrl', function($scope,$stateParams,$http,$q,$state) {
     $scope.goodName = $stateParams.goodNameoilFence;
